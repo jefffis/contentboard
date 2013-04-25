fa66bba2c22984b011e0c5039dbc534c066b5845
Jeff Doan <jeffdoan@Jeffs-MacBook-Pro-2.local>
2013-04-25 10:13:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contentboardd::Application.config.secret_token = 'ef2631eaad5d726c176582ceab34ed04e36a0a22cca253f19fbe1e473e723a1dc409509e87275116792753a48531acdc32711bc37315b8896ebdeee4ef051b0b'
